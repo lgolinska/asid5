73b6039b3796ee8565dc94e46881d89181fc8ac0
Łukasz Przykłota <2004lukas@wp.pl>
2024-06-08 22:49:29 +0200

    functions module

---
+++ functions.py
@@ -0,0 +1,30 @@
+def read_data_from_file(filename):
+    with open(filename, 'r') as file:
+        max_weight = int(file.readline().strip())
+        n = int(file.readline().strip())
+        items = []
+        
+        for i in range(n):
+            line = file.readline().strip()
+            name, weight, value = line.split(',')
+            items.append((name, int(weight), int(value)))
+        
+        items = tuple(items)
+    return max_weight, n, items
+
+def printing_result(function, items):
+    fmax, best_option = function
+    items_in_knapsack = []
+    for i in range(len(items)):
+        if best_option[i] == 1:
+            items_in_knapsack.append(items[i][0])
+    print(f"Max value: {fmax}")
+    print("Items in knapsack:", ", ".join(items_in_knapsack))
+
+def print_items(max_weight, n, items):
+    print("MAX WEIGHT OF THE KNAPSACK: ", max_weight)
+    print("NUMBER OF ITEMS AVAILABLE: ", n)
+    print(f"{'NAME':<48} {'WEIGHT':<8} {'VALUE':<5}")
+    for item in items:
+        name, weight, value = item
+        print(f"{name:<48} {weight:<8} {value:<5}")
